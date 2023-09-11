23badd84099237d70b750a76177f29dfdf7f89e9
Данила Тяпкин <dtapkin6@gmail.com>
2023-09-11 14:06:29 +0300

    Добавьте файлы проекта.

---
+++ All Process Sort/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var myProcess = from proc in Process.GetProcesses(".")
+                        // Сортировка по приоритету
+                        orderby proc.BasePriority select proc;
+        Console.WriteLine("\n*** Текущие процессы ***\n");
+        foreach (var p in myProcess)
+            Console.WriteLine("-> PID: {0}\tName: {1}", p.Id, p.ProcessName);
+    }
+}
